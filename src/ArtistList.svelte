9402430ab1a8e4091db672865ba6a300c068d522
hp <post@hpeide.no>
2023-04-24 16:40:22 +0200

    added data feed

--- src/ArtistList.svelte
+++ src/ArtistList.svelte
@@ -1,12 +1,19 @@
 <script>
     export let artistList;
-    let name = "Hans";
-    let rekon = 'testing';
 </script>
 
-{#each artistList as {name, reknown}}
-<div class="container">
-    <h2>{name}</h2>
-    <p>{reknown}</p>
+{#each artistList as {title, body}}
+<div class="artist">
+    <h2>{title}</h2>
+    <p>{body}</p>
 </div>   
+{:else}
+    <span>No data</span>
 {/each}
+
+<style>
+.artist {
+    margin-bottom: 0.5em;
+    border-bottom: 1px grey;
+}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,21 @@
 <script>
+    import {onMount} from "svelte"
     import ArtistList from "../ArtistList.svelte";
     import ArtistSearch from "../ArtistSearch.svelte";
 
+    const endpoint = "https://jsonplaceholder.typicode.com/posts";
+  
     let searchTerm = "hllo world";
-    let artistList = [
-        {
-            name: "Hans",
-            reknown: "testing"
-        },{
-            name: "Ola",
-            reknown: "testing"
-        },{
-            name: "Joachim",
-            reknown: "testing"
-        }
-    ]
+    let artistList = [];
+
+    onMount(
+      async()  => {
+        
+        const response = await fetch(endpoint);
+        artistList = await response.json();
+        console.log(artistList);
+}
+    ) 
 </script>
 
 <ArtistSearch {searchTerm} />
