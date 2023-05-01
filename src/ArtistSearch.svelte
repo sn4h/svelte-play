c80a05289355d09a68c73626369c6d7361bffdf6
hp <post@hpeide.no>
2023-05-01 14:05:34 +0200

    Reformated. Added .idea to ignore list.

--- src/ArtistSearch.svelte
+++ src/ArtistSearch.svelte
@@ -1,5 +1,6 @@
 <script>
-    import { createEventDispatcher } from 'svelte';
+    import {createEventDispatcher} from 'svelte';
+
     const dispatch = createEventDispatcher();
     export let searchTerm;
 </script>
@@ -9,19 +10,19 @@
         Search:
         <small>
             {#if searchTerm}
-            for:
+                for:
             {/if}
             {searchTerm}
         </small>
     </h4>
     <div class="SearchInput">
-        <input 
-            type="text"
-            aria-label="Search Input"
-            bind:value={searchTerm}
-            on:keyup={() => {
+        <input
+                type="text"
+                aria-label="Search Input"
+                bind:value={searchTerm}
+                on:keyup={() => {
                 dispatch('updateSearch');
             }}
-        /> 
+        />
     </div>
 </div>
